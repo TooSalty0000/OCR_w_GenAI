d078f0f3902b27ac7ee3238c90a595c1f0ab17a3
TooSalty <12saltyh@gmail.com>
2025-02-14 13:50:42 +0900

    fix cli caller name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pdfgenius",
-    version="0.1.0",
+    version="0.1.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(
@@ -23,7 +23,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "extract-pdf=pdfgenius.cli:main",
+            "pdfgenius=pdfgenius.cli:main",
         ],
     },
     python_requires=">=3.7",
